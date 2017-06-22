9440e2324cdbe68565ae7423c0f60fff82988596
nmbrodnax <nmbrodnax@gmail.com>
2017-06-21 20:35:14 -0400

    removed duplicate records

--- gender.R
+++ gender.R
@@ -10,16 +10,17 @@ library(tidyr)
 
 # load data
 personnel <- read.csv("output.csv", header = TRUE)
+personnel <- unique(personnel)
 
 # convert first name data to character datatype, make uppercase
-personnel$first_name <- as.character(toupper(personnel$first_name))
+personnel$first_name <- toupper(as.character(personnel$first_name))
 
 # create birth year min/max (assuming personnel are ages 18 to 100 at filing)
 personnel$birth_min <- as.character(personnel$fy - 100)
 personnel$birth_max <- as.character(personnel$fy - 17)
 
 # make a list with one of each name, make uppercase
-names <- unique(toupper(personnel$first_name))
+names <- toupper(unique(personnel$first_name))
 
 # predict gender using each method (excluding genderize due to 1k name limit)
 gender_ssa <- gender(names, method = "ssa") 
@@ -33,7 +34,7 @@ gender_data <- merge(gender_data, gender_ipums[,c(1,4)],
                      by.x = "names", by.y = "name", all.x=TRUE)
 gender_data <- merge(gender_data, gender_kant,
                      by.x = "names", by.y = "name", all.x=TRUE)
-colnames(gender_data) <- c("name", "ssa", "ipums", "kantrowitz")
+colnames(gender_data) <- c("first_name", "ssa", "ipums", "kantrowitz")
 
 # choose gender based on most common result across methods
 predicted_gender <- apply(gender_data[,c(2:4)], 1,
@@ -52,9 +53,11 @@ unmatched_names <- gender_data[which(gender_data$gender == 'either' |
 
 # merge personnel and gender data by matching first names
 personnel$first_name <- as.character(personnel$first_name)
-data <- merge(personnel, gender_data[,c(1,5)], by.x = "first_name", 
-              by.y = "name", all.x = TRUE)
+gender_data$first_name <- as.character(gender_data$first_name)
+personnel_gender_data <- merge(personnel, gender_data[,c(1,5)], 
+                               by = "first_name", all.x = TRUE)
+personnel_gender_data <- unique(personnel_gender_data)
 
 # export personnel and unmatched name data to csv files
-write.csv(data, "gender_output.csv", row.names = FALSE)
+write.csv(personnel_gender_data, "gender_output.csv", row.names = FALSE)
 write.csv(unmatched_names, "unmatched_names.csv", row.names = FALSE)
